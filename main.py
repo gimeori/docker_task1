ea9a5354c2efb2aae5ecc52bc01045ea04ff9c62
SEA <elizavetka07@gmail.com>
2024-03-25 14:12:31 +0700

    middleware

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-import os
-import json
-import datetime
-from flask import Flask
 
-app=Flask(__name__)
-BASE_FOLDER=os.path.dirname(os.path.abspath(__file__))
-RESOURCE_DIR=os.path.join(BASE_FOLDER, "resources")
+from fastapi import FastAPI, BackgroundTasks
+from routes import router
+import uvicorn
+from datetime import datetime
+import asyncio
+from fastapi.middleware.wsgi import WSGIMiddleware
+from datetime import datetime
+import logging
 
-@app.route('/')
-def timenow():
-    with open(os.path.join(RESOURCE_DIR,"response.json")) as f:
-        return "%s -%s" %(json.loads(f.read()).get("payload"),datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
-    
+logging.basicConfig(filename='requests.log',level=logging.INFO)
+
+app=FastAPI()
+app.include_router(router)
+
+async def time_now():
+    while True:
+        time=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f'Текущее время: {time}')
+        await asyncio.sleep(10)
+
+@app.get("/get_time")
+async def get_time(background_tasks: BackgroundTasks):
+    background_tasks.add_task(time_now)
+    return {"time":datetime.now()}
+
+@app.middleware('http')
+async def logs(request,call):
+    start_time=datetime.now()
+    response=await call(request)
+    process_time=datetime.now() - start_time
+    logging.info(f'{request.method} {request.url} - {response.status_code}')
+    return response
+
+
+
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    uvicorn.run(host="0.0.0.0", port=5000, log_level="info")
